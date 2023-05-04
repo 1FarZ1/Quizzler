da47fcb3426e60d39fb2d93833214522a2ed564e
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-04 07:44:33 +0100

    added login

--- lib/repository/user/user_repo.dart
+++ lib/repository/user/user_repo.dart
@@ -1,6 +1,7 @@
 import 'package:quizzler/data/api_service.dart';
 
 abstract class UserRepo {
+  Future<List<Map<String, dynamic>>?> logIn();
   Future<List<Map<String, dynamic>>?> getUserData();
 }
 
@@ -10,6 +11,15 @@ class UserRepoImpl implements UserRepo {
   UserRepoImpl(this.apiService);
   @override
   Future<List<Map<String, dynamic>>?> getUserData() async {
+    try {
+      final response = await apiService.getQuizzByCategorie(category: "23");
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+  @override
+  Future<List<Map<String, dynamic>>?> login() async {
     try {
       final response = await apiService.getQuizzByCategorie(category: "23");
       return response.data;
